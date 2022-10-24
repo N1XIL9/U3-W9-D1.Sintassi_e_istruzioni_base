387043933beb3a16aaf0a0d808b6e0ea98a952b8
Nicola Lerra <github.nicolalerra@gmail.com>
2022-10-24 18:28:41 +0200

    com

---
+++ assets/js/test.js
@@ -0,0 +1,31 @@
+"use strict";
+// creo variabile playerOne
+let playerOne = 4;
+console.log("PlayerOne: " + playerOne);
+// creo variabile playerTwo
+let playerTwo = 10;
+console.log("PlayerTwo: " + playerTwo);
+//Creo variabile per generare numeri rundom e con "math.floor" li arrotondo
+let pc = Math.floor((Math.random() * 100) + 1);
+console.log(pc);
+//creo la funzione per confrontare i player One-Two 
+function match() {
+    let diff1 = Math.abs(pc - playerOne);
+    let diff2 = Math.abs(pc - playerTwo);
+    if (playerOne === pc) {
+        console.log("Player One WIN!!");
+    }
+    else if (playerTwo === pc) {
+        console.log("Player Two WIN");
+    }
+    else if (diff1 < diff2) {
+        console.log("Player One gets closer");
+    }
+    else if (diff1 === diff2) {
+        console.log("No one comes closer");
+    }
+    else {
+        console.log("Playere Two gets closer");
+    }
+}
+console.log(match);
